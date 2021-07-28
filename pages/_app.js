25cbc37a454cd82331c2bb6e2095bd2777d42ee8
Devanshi Desai <contact.ddevanshi@gmail.com>
2021-07-28 16:02:07 +0530

    manifest added

--- pages/_app.js
+++ pages/_app.js
@@ -12,6 +12,7 @@ function MyApp({ Component, pageProps }) {
       <Layout>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+          <link rel="manifest" href="/manifest.json" />
         </Head>
         <Component {...pageProps} />
       </Layout>
